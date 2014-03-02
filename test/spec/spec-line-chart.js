3116831cc11bc2ba54886106580e2f221956fda3
Gion Kunz <gion.kunz@ctp.com>
2014-03-02 20:12:38 +0100

    Removed Karma and added direct jasmine dependency

---
+++ test/spec/spec-line-chart.js
@@ -0,0 +1,15 @@
+describe('Line chart tests', function() {
+  'use strict';
+
+  beforeEach(function() {
+
+  });
+
+  afterEach(function() {
+
+  });
+
+  it('should not give an error because we have 0% test coverage ;-)', function() {
+    expect(true).toBe(true);
+  });
+});
