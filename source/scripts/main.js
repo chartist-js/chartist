e4316d6815d478fdc67b1f4682a8badf772dbf04
Gion Kunz <gion.kunz@ctp.com>
2014-03-10 00:15:33 +0100

    Merge 0.2

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,25 +1,86 @@
 'use strict';
 
 var optionsChartistGuy = {
-  chartPadding: 10,
   axisX: {
-    labelOffset: 15
+    showLabel: true,
+    showGrid: true,
+    labelInterpolationFnc: function(n) {
+      return n;
+    }
+  },
+  axisY: {
+    showLabel: true,
+    showGrid: true,
+    labelInterpolationFnc: function(n) {
+      return Math.round(n / 100000) / 10 + 'm.';
+    }
   }
 };
 
 var chartistGuyData = {
-  labels: ['One', 'Two', 'Three', 'Four'],
+  labels: ['1st', '2nd', '3rd'],
   series: [
     {
-      data: [1883000, 2050000, 3706000, 5806000]
+      data: [1283000, 1500000, 5706000]
     },
     {
-      data: [1283000, 3250000, 5706000, 3950000]
+      data: [1883000, 2050000, 3706000]
     }
   ]
 };
 
 var examples = [
+  {
+    data: {
+      labels: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      series: [
+        {
+          data: [12, 8, 5, 7, 2, 4, 3, 2, 10]
+        },
+        {
+          data: [2, 1, 4, 7, 9, 8, 7, 4, 7]
+        },
+        {
+          data: [1, 2, 5, 3, 9, 2, 3, 2, 4]
+        },
+        {
+          data: [7, 5, 3, 2, 1, 0, 2, 5, 9]
+        }
+      ]
+    },
+    options: {
+      axisX: {
+        offset: 40,
+        labelInterpolationFnc: function(value) {
+          return 'Week ' + value;
+        }
+      },
+      axisY: {
+        offset: 40
+      }
+    },
+    responsiveOptions: [
+      [
+        Foundation.media_queries.small,
+        {
+          axisX: {
+            offset: 5,
+            labelInterpolationFnc: function(value, index) {
+              // Interpolation function causes only every 2nd label to be displayed
+              if(index % 2 !== 0) {
+                return false;
+              } else {
+                return 'W' + value;
+              }
+            }
+          },
+          axisY: {
+            offset: 5
+          }
+        }
+      ]
+    ]
+  },
   {
     data: {
       labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
@@ -35,15 +96,12 @@ var examples = [
         }
       ]
     },
-    options: {
-    },
+    options: {},
     responsiveOptions: [
       [
         Foundation.media_queries.small,
         {
-          labelSampling: 2,
           axisX: {
-            labelOffset: 20,
             labelInterpolationFnc: function(value, index) {
               // Interpolation function causes only every 2nd label to be displayed
               if(index % 2 !== 0) {
@@ -73,6 +131,7 @@ Snap.load($('#chartist-guy').data('svgSrc'), function (fragment) {
 
 // Initialize test line chart with override settings for small breakpoint
 charts.push(window.Chartist('#example-chart-one', examples[0].data, examples[0].options, examples[0].responsiveOptions));
+charts.push(window.Chartist('#example-chart-two', examples[1].data, examples[1].options, examples[1].responsiveOptions));
 // On window resize trigger reflow
 $(window).on('resize', function () {
   console.log('Resize');
