aa194d8c4d08c3ab58357dbded7f8870e9af55d2
Gion Kunz <gion.kunz@ctp.com>
2014-04-06 21:52:47 +0200

    Updated bower-install

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function (grunt) {
     watch: {
       assemble: {
         files: ['<%= pkg.config.source %>/docs/{,*/}*.{hbs,yml,json}'],
-        tasks: ['assemble', 'bower-install']
+        tasks: ['assemble', 'bowerInstall']
       },
       js: {
         files: ['<%= pkg.config.source %>/scripts/{,*/}*.js'],
@@ -145,9 +145,9 @@ module.exports = function (grunt) {
     },
 
     // Automatically inject Bower components into the app
-    'bower-install': {
+    'bowerInstall': {
       app: {
-        html: '.tmp/index.html',
+        src: ['.tmp/**/*.html'],
         ignorePath: '<%= pkg.config.source %>/',
         exclude: '<%= pkg.config.bowerExclude %>'
       }
@@ -403,7 +403,7 @@ module.exports = function (grunt) {
     grunt.task.run([
       'clean:server',
       'assemble',
-      'bower-install',
+      'bowerInstall',
       'concurrent:server',
       'connect:livereload',
       'watch'
@@ -425,7 +425,7 @@ module.exports = function (grunt) {
   grunt.registerTask('build', [
     'clean:dist',
     'assemble',
-    'bower-install',
+    'bowerInstall',
     'useminPrepare',
     'concurrent:dist',
     'concat',
