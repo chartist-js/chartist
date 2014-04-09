d16f4178e00902444e58c9999a079bae8d8ec801
Gion Kunz <gion.kunz@ctp.com>
2014-04-09 21:52:58 +0200

    Added script to pull documentation (dist) into documentation branch root

---
+++ doc-clean-pull.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Delete all resources and get fresh copy from dist into the documentation root
+rm -rf bower_components
+rm -rf fonts
+rm -rf images
+rm -rf scripts
+rm -rf styles
+cp -r dist/* .
