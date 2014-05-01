2000b14f2cdce2f7dab61bd39f7d7ca4b0ff1458
Gion Kunz <gion.kunz@ctp.com>
2014-05-01 13:58:55 +0200

    Added bar chart js

---
+++ source/scripts/chartist.bar.js
@@ -0,0 +1,198 @@
+// Chartist Bar chart
+(function (document, window, Chartist, undefined) {
+  'use strict';
+  Chartist.Bar = Chartist.Bar || function (query, data, options, responsiveOptions) {
+
+    var defaultOptions = {
+        axisX: {
+          offset: 10,
+          showLabel: true,
+          showGrid: true,
+          labelInterpolationFnc: Chartist.noop
+        },
+        axisY: {
+          offset: 15,
+          showLabel: true,
+          showGrid: true,
+          labelAlign: 'right',
+          labelInterpolationFnc: Chartist.noop,
+          scaleMinSpace: 20
+        },
+        width: undefined,
+        height: undefined,
+        chartPadding: 5,
+        classNames: {
+          label: 'ct-label',
+          series: 'ct-series',
+          bar: 'ct-bar',
+          grid: 'ct-grid',
+          vertical: 'ct-vertical',
+          horizontal: 'ct-horizontal'
+        }
+      },
+      currentOptions,
+      paper,
+      dataArray = Chartist.normalizeDataArray(Chartist.getDataArray(data), data.labels.length);
+
+    function createChart(options) {
+      var xAxisOffset,
+        yAxisOffset,
+        seriesGroups = [],
+        bounds;
+
+      // Create new paper the stage
+      paper = Chartist.createPaper(query, options.width, options.height);
+
+      // initialize bounds
+      bounds = Chartist.getBounds(paper, dataArray, options);
+
+      xAxisOffset = options.axisX.offset;
+      if (options.axisX.showLabel) {
+        xAxisOffset += Chartist.calculateLabelOffset(
+          paper,
+          data.labels,
+          [options.classNames.label, options.classNames.horizontal].join(' '),
+          options.axisX.labelInterpolationFnc,
+          Chartist.getHeight
+        );
+      }
+
+      yAxisOffset = options.axisY.offset;
+      if (options.axisY.showLabel) {
+        yAxisOffset += Chartist.calculateLabelOffset(
+          paper,
+          bounds.values,
+          [options.classNames.label, options.classNames.horizontal].join(' '),
+          options.axisY.labelInterpolationFnc,
+          Chartist.getWidth
+        );
+      }
+
+      var chartRect = Chartist.createChartRect(paper, options, xAxisOffset, yAxisOffset);
+      // Start drawing
+      var labels = paper.g(),
+        grid = paper.g();
+
+      // Create X-Axis
+      Chartist.each(data.labels, function (index, value) {
+        var interpolatedValue = options.axisX.labelInterpolationFnc(value, index),
+          pos = chartRect.x1 + chartRect.width() / data.labels.length * index;
+
+        // If interpolated value returns falsey (except 0) we don't draw the grid line
+        if(!interpolatedValue && interpolatedValue !== 0) {
+          return;
+        }
+
+        if (options.axisX.showGrid) {
+          var line = paper.line(pos, chartRect.y1, pos, chartRect.y2);
+          line.node.setAttribute('class', [options.classNames.grid, options.classNames.horizontal].join(' '));
+          grid.add(line);
+        }
+
+        if (options.axisX.showLabel) {
+          // Use config offset for setting labels of
+          var label = paper.text(pos + 2, 0, '' + interpolatedValue);
+          label.node.setAttribute('class', [options.classNames.label, options.classNames.horizontal].join(' '));
+
+          // TODO: should use 'alignment-baseline': 'hanging' but not supported in firefox. Instead using calculated height to offset y pos
+          label.attr({
+            y: chartRect.y1 + Chartist.getHeight(label.node) + options.axisX.offset
+          });
+
+          labels.add(label);
+        }
+      });
+
+      // Create Y-Axis
+      Chartist.each(bounds.values, function (index, value) {
+        var interpolatedValue = options.axisY.labelInterpolationFnc(value, index),
+          pos = chartRect.y1 - chartRect.height() / bounds.values.length * index;
+
+        // If interpolated value returns falsey (except 0) we don't draw the grid line
+        if(!interpolatedValue && interpolatedValue !== 0) {
+          return;
+        }
+
+        if (options.axisY.showGrid) {
+          var line = paper.line(chartRect.x1, pos, chartRect.x2, pos);
+          line.node.setAttribute('class', options.classNames.grid + ' ' + options.classNames.vertical);
+          grid.add(line);
+        }
+
+        if (options.axisY.showLabel) {
+          // Position later
+          //TODO: make padding of 2px configurable
+          var label = paper.text(options.axisY.labelAlign === 'right' ? yAxisOffset - options.axisY.offset + options.chartPadding : options.chartPadding,
+            pos - 2, '' + interpolatedValue);
+          label.node.setAttribute('class', options.classNames.label + ' ' + options.classNames.vertical);
+
+          // Set text-anchor based on alignment
+          label.attr({
+            'text-anchor': options.axisY.labelAlign === 'right' ? 'end' : 'start'
+          });
+
+          labels.add(label);
+        }
+      });
+
+      function projectPoint(data, index) {
+        return {
+          x: chartRect.x1 + chartRect.width() / data.length * index,
+          y: chartRect.y1 - chartRect.height() * (data[index] - bounds.min) / (bounds.range + bounds.step)
+        };
+      }
+
+      function createSeries(seriesGroups, data, paper, options) {
+        // Draw the series
+        // initialize series groups
+        for (var i = 0; i < data.series.length; i++) {
+          seriesGroups[i] = paper.g();
+          // Use series class from series data or if not set generate one
+          seriesGroups[i].node.setAttribute('class', options.classNames.series + ' ' +
+            (data.series[i].className || options.classNames.series + '-' + Chartist.alphaNumerate(i)));
+
+          for(var j = 0; j < data.series[i].data.length; j++) {
+            var p = projectPoint(data.series[i].data, j),
+              bar;
+
+            // Offset to center bar between grid lines.
+            p.x += chartRect.width() / data.series[i].data.length / 2;
+
+            bar = paper.line(p.x, chartRect.y1, p.x, p.y);
+            bar.node.setAttribute('class', options.classNames.bar);
+            seriesGroups[i].prepend(bar);
+          }
+
+          paper.add(seriesGroups[i]);
+        }
+      }
+
+      createSeries(seriesGroups, data, paper, options);
+    }
+
+    // Obtain current options based on matching media queries (if responsive options are given)
+    // This will also register a listener that is re-creating the chart based on media changes
+    currentOptions = Chartist.optionsProvider(defaultOptions, options, responsiveOptions, function (changedOptions) {
+      currentOptions = changedOptions;
+      createChart(currentOptions);
+    });
+
+    // TODO: Currently we need to re-draw the chart on window resize. This is usually very bad and will affect performance.
+    // This is done because we can't work with relative coordinates when drawing the chart because SVG Path does not
+    // work with relative positions yet. We need to check if we can do a viewBox hack to switch to percentage.
+    // See http://mozilla.6506.n7.nabble.com/Specyfing-paths-with-percentages-unit-td247474.html
+    // Update: can be done using the above method tested here: http://codepen.io/gionkunz/pen/KDvLj
+    // The problem is with the label offsets that can't be converted into percentage and affecting the chart container
+    window.addEventListener('resize', function () {
+      createChart(currentOptions);
+    });
+
+    // Public members
+    return {
+      version: Chartist.version,
+      update: function () {
+        createChart(currentOptions);
+      }
+    };
+  };
+}(document, window, window.Chartist));
