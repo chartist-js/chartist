c70b50afd134b1c02c96b2ac4211241fd520a90a
Gion Kunz <gion.kunz@ctp.com>
2014-06-16 23:13:08 +0200

    Updated documentation

---
+++ source/site/code-snippets/simple-start-fixed-chart.js
@@ -0,0 +1,24 @@
+var data = {
+  // A labels array that can contain any sort of values
+  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+  // Our series array that contains series objects
+  series: [
+    // A series object consists of data and meta information about the series
+    {
+      // Our data array contains numeric data
+      data: [5, 2, 4, 2, 0]
+    }
+  ]
+};
+
+// As options we currently only set a static size of 300x200 px. We can also omit this and use flexible containers
+// which you'll learn later on
+var options = {
+  width: 300,
+  height: 200
+};
+
+// In the global name space Chartist we call the Line function to initialize a line chart
+// As a first parameter we pass in a selector where we would like to get our chart created
+// Second parameter is the actuall data object and as a third parameter we pass in our options
+Chartist.Line('.ct-chart', data, options);
