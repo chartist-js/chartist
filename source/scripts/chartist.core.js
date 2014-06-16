02c4e1bdabac97e78ac479fbd01030873297dcb4
Gion Kunz <gion.kunz@ctp.com>
2014-06-16 23:06:11 +0200

    Fixed version in chartist core js

--- source/scripts/chartist.core.js
+++ source/scripts/chartist.core.js
@@ -4,7 +4,7 @@
   // Export chartist namespace
   var Chartist = window.Chartist = window.Chartist || {};
 
-  Chartist.version = '0.0.3';
+  Chartist.version = '0.1.2';
 
   // Helps to simplify functional style code
   Chartist.noop = function (n) {
