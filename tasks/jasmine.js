99fe27ba5b8ec083fc76d36b176278a166f40ab1
Gion Kunz <gion.kunz@gmail.com>
2014-12-11 23:37:03 +0100

    Fixed test paths

--- tasks/jasmine.js
+++ tasks/jasmine.js
@@ -13,14 +13,14 @@ module.exports = function (grunt) {
   return {
     dist: {
       src: [
-        '<%= pkg.config.src %>/core.js',
-        '<%= pkg.config.src %>/event.js',
-        '<%= pkg.config.src %>/class.js',
-        '<%= pkg.config.src %>/base.js',
-        '<%= pkg.config.src %>/svg.js',
-        '<%= pkg.config.src %>/types/line.js',
-        '<%= pkg.config.src %>/types/bar.js',
-        '<%= pkg.config.src %>/types/pie.js'
+        '<%= pkg.config.src %>/scripts/core.js',
+        '<%= pkg.config.src %>/scripts/event.js',
+        '<%= pkg.config.src %>/scripts/class.js',
+        '<%= pkg.config.src %>/scripts/base.js',
+        '<%= pkg.config.src %>/scripts/svg.js',
+        '<%= pkg.config.src %>/scripts/charts/line.js',
+        '<%= pkg.config.src %>/scripts/charts/bar.js',
+        '<%= pkg.config.src %>/scripts/charts/pie.js'
       ],
       options: {
         specs: '<%= pkg.config.test %>/spec/**/spec-*.js',
