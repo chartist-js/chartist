e3f65d32849f2a344489f9ee2b965a90a1c3e65e
Gion Kunz <gion.kunz@gmail.com>
2015-02-03 19:54:51 +0100

    Removed unused Class function

--- src/scripts/class.js
+++ src/scripts/class.js
@@ -85,111 +85,6 @@
     return constr;
   }
 
-  /**
-   * Creates a mixin from multiple super prototypes.
-   *
-   * @memberof Chartist.Class
-   * @param {Array} mixProtoArr An array of super prototypes or an array of super prototype constructors.
-   * @param {Object} properties The object that serves as definition for the prototype that gets created for the new class. This object should always contain a constructor property that is the desired constructor for the newly created class.
-   * @return {Function} Constructor function of the newly created mixin class
-   *
-   * @example
-   * var Fruit = Class.extend({
-     * color: undefined,
-     *   sugar: undefined,
-     *
-     *   constructor: function(color, sugar) {
-     *     this.color = color;
-     *     this.sugar = sugar;
-     *   },
-     *
-     *   eat: function() {
-     *     this.sugar = 0;
-     *     return this;
-     *   }
-     * });
-   *
-   * var Banana = Fruit.extend({
-     *   length: undefined,
-     *
-     *   constructor: function(length, sugar) {
-     *     Banana.super.constructor.call(this, 'Yellow', sugar);
-     *     this.length = length;
-     *   }
-     * });
-   *
-   * var banana = new Banana(20, 40);
-   * console.log('banana instanceof Fruit', banana instanceof Fruit);
-   * console.log('Fruit is prototype of banana', Fruit.prototype.isPrototypeOf(banana));
-   * console.log('bananas prototype is Fruit', Object.getPrototypeOf(banana) === Fruit.prototype);
-   * console.log(banana.sugar);
-   * console.log(banana.eat().sugar);
-   * console.log(banana.color);
-   *
-   *
-   * var KCal = Class.extend({
-     *   sugar: 0,
-     *
-     *   constructor: function(sugar) {
-     *     this.sugar = sugar;
-     *   },
-     *
-     *   get kcal() {
-     *     return [this.sugar * 4, 'kcal'].join('');
-     *   }
-     * });
-   *
-   *      var Nameable = Class.extend({
-     *   name: undefined,
-     *
-     *   constructor: function(name) {
-     *     this.name = name;
-     *   }
-     * });
-   *
-   * var NameableKCalBanana = Class.mix([Banana, KCal, Nameable], {
-     *   constructor: function(name, length, sugar) {
-     *     Nameable.prototype.constructor.call(this, name);
-     *     Banana.prototype.constructor.call(this, length, sugar);
-     *   },
-     *
-     *   toString: function() {
-     *     return [this.name, 'with', this.length + 'cm', 'and', this.kcal].join(' ');
-     *   }
-     * });
-   *
-   *
-   *
-   * var banana = new Banana(20, 40);
-   * console.log('banana instanceof Fruit', banana instanceof Fruit);
-   * console.log('Fruit is prototype of banana', Fruit.prototype.isPrototypeOf(banana));
-   * console.log('bananas prototype is Fruit', Object.getPrototypeOf(banana) === Fruit.prototype);
-   * console.log(banana.sugar);
-   * console.log(banana.eat().sugar);
-   * console.log(banana.color);
-   *
-   * var superBanana = new NameableKCalBanana('Super Mixin Banana', 30, 80);
-   * console.log(superBanana.toString());
-   *
-   */
-  function mix(mixProtoArr, properties) {
-    if(this !== Chartist.Class) {
-      throw new Error('Chartist.Class.mix should only be called on the type and never on an instance!');
-    }
-
-    // Make sure our mixin prototypes are the class objects and not the constructors
-    var superPrototypes = [{}]
-      .concat(mixProtoArr)
-      .map(function (prototype) {
-        return prototype instanceof Function ? prototype.prototype : prototype;
-      });
-
-    var mixedSuperProto = Chartist.Class.cloneDefinitions.apply(undefined, superPrototypes);
-    // Delete the constructor if present because we don't want to invoke a constructor on a mixed prototype
-    delete mixedSuperProto.constructor;
-    return this.extend(properties, mixedSuperProto);
-  }
-
   // Variable argument list clones args > 0 into args[0] and retruns modified args[0]
   function cloneDefinitions() {
     var args = listToArray(arguments);
@@ -210,7 +105,6 @@
 
   Chartist.Class = {
     extend: extend,
-    mix: mix,
     cloneDefinitions: cloneDefinitions
   };
 
