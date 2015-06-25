968dd8f06ead776ca7fcd5aa744fb411f25022ad
alexstanbury <alexstanbury@googlemail.com>
2015-06-25 10:09:26 +0100

    Updated example and bower config.

--- site/examples/example-plugin-axistitle.js
+++ site/examples/example-plugin-axistitle.js
@@ -1,36 +1,36 @@
 new Chartist.Line('.ct-chart', {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-  series: [
-    {name: 'Income', data: [20000, 30000, 35000, 32000, 40000, 42000, 50000, 62000, 80000, 94000, 100000, 120000]},
-    {name: 'Expenses', data: [10000, 15000, 12000, 14000, 20000, 23000, 22000, 24000, 21000, 18000, 30000, 32000]}
-  ]
+    labels: ['0-15', '16-30', '31-45', '46-60', '61-75', '76-90', '91-105', '106-120'],
+    series: [1, 3, 7, 12, 1, 2, 1, 0]
 }, {
-  fullWidth: true,
-  lineSmooth: false,
-  chartPadding: {
-    right: 20,
-    left: 10
-  },
-  axisX: {
-    labelInterpolationFnc: function(value) {
-      return value.split('').slice(0, 3).join('');
-    }
-  },
-  plugins: [
-    Chartist.plugins.ctAccessibility({
-      caption: 'Fiscal year 2015',
-      seriesHeader: 'business numbers',
-      summary: 'A graphic that shows the business numbers of the fiscal year 2015',
-      valueTransform: function(value) {
-        return value + ' dollar';
-      },
-      // ONLY USE THIS IF YOU WANT TO MAKE YOUR ACCESSIBILITY TABLE ALSO VISIBLE!
-      visuallyHiddenStyles: 'position: absolute; top: 100%; width: 100%; font-size: 11px; overflow-x: auto; background-color: rgba(0, 0, 0, 0.1); padding: 10px'
-    })
-  ]
-});
-
-// This is only used for the example on the Chartist example page
-$chart.parent().css({
-  'margin-bottom': '160px'
+    chartPadding: {
+        top: 20,
+        right: 0,
+        bottom: 20,
+        left: 0
+    },
+    axisY: {
+        onlyInteger: true
+    },
+    plugins: [
+        Chartist.plugins.ctAxisTitle({
+            axisX: {
+                axisTitle: 'Time (mins)',
+                axisClass: 'ct-axis-title',
+                offset: {
+                    x: 0,
+                    y: 50
+                },
+                textAnchor: 'middle'
+            },
+            axisY: {
+                axisTitle: 'Goals',
+                axisClass: 'ct-axis-title',
+                offset: {
+                    x: 0,
+                    y: 0
+                },
+                flipTitle: false
+            }
+        })
+    ]
 });
