e38aa2757d502431d3ecd7e09770249a4d42d4da
hansmaad <hansmaad@gmail.com>
2016-03-18 14:59:23 +0100

    Add unit tests for Chartist.getBounds

--- test/spec/spec-core.js
+++ test/spec/spec-core.js
@@ -302,4 +302,91 @@ describe('Chartist core', function() {
     });
     
   });
+  
+  describe('getBounds', function() {
+    
+    it('should return 10 steps', function() {
+      var bounds = Chartist.getBounds(100, { high: 10, low: 1 }, 10, false);
+      expect(bounds.min).toBe(1);
+      expect(bounds.max).toBe(10);
+      expect(bounds.values).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    });  
+
+    it('should return 5 steps', function() {
+      var bounds = Chartist.getBounds(100, { high: 10, low: 1 }, 20, false);
+      expect(bounds.min).toBe(1);
+      expect(bounds.max).toBe(10);
+      expect(bounds.values).toEqual([1, 3, 5, 7, 9]);
+      // Is this correct behaviour? Should it include 10?
+    });  
+    
+    it('should return non integer steps', function() {
+      var bounds = Chartist.getBounds(100, { high: 2, low: 1 }, 20, false);
+      expect(bounds.min).toBe(1);
+      expect(bounds.max).toBe(2);
+      expect(bounds.values).toEqual([ 1, 1.25, 1.5, 1.75, 2 ]);
+    });
+    
+    it('should return integer steps only', function() {
+      var bounds = Chartist.getBounds(100, { high: 3, low: 1 }, 20, true);
+      expect(bounds.min).toBe(1);
+      expect(bounds.max).toBe(3);
+      expect(bounds.values).toEqual([ 1, 2, 3 ]);
+    });
+    
+    it('should return single integer step', function() {
+      var bounds = Chartist.getBounds(100, { high: 2, low: 1 }, 20, true);
+      expect(bounds.min).toBe(1);
+      expect(bounds.max).toBe(2);
+      expect(bounds.values).toEqual([ 1, 2,]);
+    });
+    
+    it('should floor/ceil min/max', function() {
+      var bounds = Chartist.getBounds(100, { high: 9.9, low: 1.01 }, 20, false);
+      expect(bounds.min).toBe(1);
+      expect(bounds.max).toBe(10);
+      expect(bounds.values).toEqual([1, 3, 5, 7, 9]);
+      // Is this correct behaviour? Should it include 10?
+    });
+    
+    it('should floor/ceil min/max for non integers', function() {
+      var bounds = Chartist.getBounds(100, { high: 2.9, low: 1.01 }, 20, false);
+      expect(bounds.min).toBe(1);
+      expect(bounds.max).toBe(3);
+      expect(bounds.values).toEqual([1, 1.5, 2, 2.5, 3]);
+    });
+    
+    it('should floor/ceil min/max if integers only', function() {
+      var bounds = Chartist.getBounds(100, { high: 2.9, low: 1.01 }, 20, true);
+      expect(bounds.min).toBe(1);
+      expect(bounds.max).toBe(3);
+      expect(bounds.values).toEqual([1, 2, 3]);
+    });
+    
+    it('should return neg and pos values', function() {
+      var bounds = Chartist.getBounds(100, { high: 1.9, low: -0.9 }, 20, false);
+      expect(bounds.min).toBe(-1);
+      expect(bounds.max).toBe(2);
+      expect(bounds.values).toEqual([-1, 0, 1, 2]);
+    });
+    
+    it('should return two steps if no space', function() {
+      var bounds = Chartist.getBounds(100, { high: 5, low: 0 }, 45, false);
+      expect(bounds.min).toBe(0);
+      expect(bounds.max).toBe(5);
+      expect(bounds.values).toEqual([0, 4]);
+      // Is this correct behaviour? Should it be [0, 5]?
+    });
+    
+    it('should return single step if no space', function() {
+      var bounds = Chartist.getBounds(100, { high: 5, low: 0 }, 80, false);
+      expect(bounds.min).toBe(0);
+      expect(bounds.max).toBe(5);
+      expect(bounds.values).toEqual([0]);
+      // Is this correct behaviour? Should it be [0, 5]?
+    });
+    
+  });
+  
+  
 });
