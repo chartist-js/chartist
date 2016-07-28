dc1f1b87397a64b50a9511e025d95dc8b7f1f0da
Rittmeyer Balz <balz.rittmeyer@nzz.ch>
2016-07-28 12:17:38 +0200

    add auto scale axis tests for new introduced 'ensureTickValue' option

--- test/spec/spec-axes.js
+++ test/spec/spec-axes.js
@@ -1,6 +1,79 @@
 describe('Axes tests', function() {
   'use strict';
 
+  describe('auto scale axis', function () {
+
+    var axisUnit, chartRect, options, genericAsAxis, otherAsAxis;
+
+    beforeEach(function() {
+      axisUnit = {
+        'pos':'y',
+        'len':'height',
+        'dir':'vertical',
+        'rectStart':'y2',
+        'rectEnd':'y1',
+        'rectOffset':'x1'
+      };
+      chartRect = {
+        'padding':{'top':15,'right':15,'bottom':5,'left':10},
+        'y2':15,
+        'y1':141,
+        'x1':50,
+        'x2':269
+      };
+      options = {
+        highLow: {
+          high: 8.2,
+          low: -1
+        }
+      };
+      genericAsAxis = new Chartist.AutoScaleAxis(axisUnit, {}, chartRect, options);
+
+    });
+
+    it('should ensure the inclusion of a tick value set in axis options', function() {
+
+      // proves the possibility of ticks not including a value like 0
+      // (0 is probably the most popular use case here)
+      expect(genericAsAxis.bounds.step).toEqual(2);
+      expect(genericAsAxis.ticks).not.toContain(0);
+      expect(genericAsAxis.ticks).toEqual([ -1, 1, 3, 5, 7, 9 ]);
+
+      // preserves step size but ensures the inclusion of zero in ticks
+      options.ensureTickValue = 0;
+      otherAsAxis = new Chartist.AutoScaleAxis(axisUnit, {}, chartRect, options);
+      expect(otherAsAxis.bounds.step).toEqual(genericAsAxis.bounds.step);
+      expect(otherAsAxis.ticks).toContain(0);
+
+    });
+
+    it('should ensure the inclusion of a floating number as well', function() {
+      options.ensureTickValue = -1.25;
+      otherAsAxis = new Chartist.AutoScaleAxis(axisUnit, {}, chartRect, options);
+      expect(otherAsAxis.bounds.step).toEqual(genericAsAxis.bounds.step);
+      expect(otherAsAxis.ticks).toContain(-1.25);
+    });
+
+    it('should re-set the bound\'s range and maximum value if the ensured tick value is larger than the calculated maximum', function() {
+      options.ensureTickValue = 250;
+      otherAsAxis = new Chartist.AutoScaleAxis(axisUnit, {}, chartRect, options);
+      expect(otherAsAxis.bounds.step).toEqual(genericAsAxis.bounds.step);
+      expect(otherAsAxis.bounds.range).toBeGreaterThan(genericAsAxis.bounds.range);
+      expect(otherAsAxis.bounds.max).toEqual(250);
+      expect(otherAsAxis.ticks).toContain(250);
+    });
+
+    it('should re-set the bound\'s range and minimum value if the ensured tick value is lower than the calculated minimum', function() {
+      options.ensureTickValue = -250;
+      otherAsAxis = new Chartist.AutoScaleAxis(axisUnit, {}, chartRect, options);
+      expect(otherAsAxis.bounds.step).toEqual(genericAsAxis.bounds.step);
+      expect(otherAsAxis.bounds.range).toBeGreaterThan(genericAsAxis.bounds.range);
+      expect(otherAsAxis.ticks).toContain(-250);
+      expect(otherAsAxis.bounds.min).toEqual(-250);
+    });
+
+  });
+
   describe('fixed scale axis', function () {
     it('should order the tick array', function() {
 
