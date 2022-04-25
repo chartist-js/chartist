6d3891a093c79effd0d26c4249e1fdb6d108c850
Dan Onoshko <danon0404@gmail.com>
2022-04-26 02:13:12 +0700

    build: renew styles build process (#7)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,22 @@
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/preset-scss',
     '@storybook/addon-docs',
     '@storybook/addon-controls',
     '@storybook/addon-actions',
-  ]
+  ],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      sideEffects: true,
+      use: [
+        'style-loader',
+        'css-loader',
+        'postcss-loader',
+        'sass-loader'
+      ].map(require.resolve)
+    });
+
+    return config;
+  },
 };
--- scripts/styles.js
+++ scripts/styles.js
@@ -1,3 +1,43 @@
 #!/usr/bin/env node
 
-require('./compile-sass')(process.argv[2], process.argv[3]);
+const fs = require('fs').promises;
+const path = require('path');
+const sass = require('sass');
+const postcss = require('postcss');
+
+const { plugins } = require('../postcss.config');
+const pkg = require('../package.json');
+
+const cwd = process.cwd();
+const input = pkg.style;
+const output = pkg.publishConfig.style;
+const sourceMapOutput = pkg.publishConfig.style.replace('.css', '.css.map');
+
+(async () => {
+  let styles
+
+  styles = sass.compile(pkg.style, {
+    sourceMap: true
+  })
+
+  styles.sourceMap.sources = styles.sourceMap.sources.map(_ => _.replace(cwd, ''))
+
+  styles = await postcss(plugins).process(styles.css, {
+    from: input,
+    to: output,
+    map: {
+      prev: styles.sourceMap
+    }
+  })
+
+  const map = styles.map.toString()
+  const css = styles.css + `\n/*# sourceMappingURL=${path.basename(sourceMapOutput)} */`
+
+  await fs.mkdir(path.dirname(output), {
+    recursive: true
+  })
+  await Promise.all([
+    fs.writeFile(output, css),
+    fs.writeFile(sourceMapOutput, map)
+  ])
+})()
