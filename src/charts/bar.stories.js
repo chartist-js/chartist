68fc818d59bd7cdb50438aa6dd5028fbfa5f4449
Dan Onoshko <danon0404@gmail.com>
2022-04-29 15:20:43 +0700

    test: add more stories (#8)

--- src/charts/bar.stories.js
+++ src/charts/bar.stories.js
@@ -1,6 +1,5 @@
 import '../styles/chartist.scss';
-import { BarChart } from './bar';
-import {AutoScaleAxis} from '../axes/axes';
+import { BarChart, AutoScaleAxis, Svg, getMultiValue } from '..';
 
 export default {
   title: 'BarChart',
@@ -8,6 +7,205 @@ export default {
 };
 
 export function Default() {
+  const root = document.createElement('div');
+
+  new BarChart(root, {
+    series: [[
+      {x: 1, y: 1},
+      {x: 3, y: 5}
+    ]]
+  }, {
+    axisX: {
+      type: AutoScaleAxis,
+      onlyInteger: true
+    },
+    axisY: {
+      type: AutoScaleAxis,
+      onlyInteger: true
+    }
+  });
+
+  return root
+}
+
+export function BiPolar() {
+  const root = document.createElement('div');
+
+  new BarChart(root, {
+    labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
+    series: [
+      [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
+    ]
+  }, {
+    high: 10,
+    low: -10,
+    axisX: {
+      labelInterpolationFnc(value, index) {
+        return index % 2 === 0 ? value : null;
+      }
+    }
+  });
+
+  return root
+}
+
+export function Labels() {
+  const root = document.createElement('div');
+
+  new BarChart(root, {
+    labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu'],
+    series: [
+      [5, 2, 4, 2, 0]
+    ]
+  }, {
+  });
+
+  return root
+}
+
+export function MultilineLabels() {
+  const root = document.createElement('div');
+
+  new BarChart(root, {
+    labels: ['First quarter of the year', 'Second quarter of the year', 'Third quarter of the year', 'Fourth quarter of the year'],
+    series: [
+      [60000, 40000, 80000, 70000],
+      [40000, 30000, 70000, 65000],
+      [8000, 3000, 10000, 6000]
+    ]
+  }, {
+    seriesBarDistance: 10,
+    axisX: {
+      offset: 60
+    },
+    axisY: {
+      offset: 80,
+      labelInterpolationFnc(value) {
+        return value + ' CHF'
+      },
+      scaleMinSpace: 15
+    }
+  });
+
+  return root
+}
+
+export function LabelsPlacement() {
+  const root = document.createElement('div');
+
+  new BarChart(root, {
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    series: [
+      [5, 4, 3, 7, 5, 10, 3],
+      [3, 2, 9, 5, 4, 6, 4]
+    ]
+  }, {
+    axisX: {
+      // On the x-axis start means top and end means bottom
+      position: 'start'
+    },
+    axisY: {
+      // On the y-axis start means left and end means right
+      position: 'end'
+    }
+  });
+
+  return root
+}
+
+export function MultiSeries() {
+  const root = document.createElement('div');
+
+  new BarChart(root, {
+    series: [
+      [1, 2, 3, 4],
+      [2, 3, 4],
+      [3, 4]
+    ]
+  }, {});
+
+  return root
+}
+
+export function DistributedSeries() {
+  const root = document.createElement('div');
+
+  new BarChart(root, {
+    labels: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+    series: [20, 60, 120, 200, 180, 20, 10]
+  }, {
+    distributeSeries: true
+  });
+
+  return root
+}
+
+export function ReverseData() {
+  const root = document.createElement('div');
+
+  new BarChart(root, {
+    series: [
+      [1, 2, 3, 4],
+      [2, 3, 4],
+      [3, 4]
+    ]
+  }, {
+    reverseData: true
+  });
+
+  return root
+}
+
+export function Stack() {
+  const root = document.createElement('div');
+
+  new BarChart(root, {
+    labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+    series: [
+      [800000, 1200000, 1400000, 1300000],
+      [200000, 400000, 500000, 300000],
+      [100000, 200000, 400000, 600000]
+    ]
+  }, {
+    stackBars: true,
+    axisY: {
+      labelInterpolationFnc(value) {
+        return (value / 1000) + 'k';
+      }
+    }
+  }).on('draw', (data) => {
+    if(data.type === 'bar') {
+      data.element.attr({
+        style: 'stroke-width: 30px'
+      });
+    }
+  })
+
+  return root
+}
+
+export function Horizontal() {
+  const root = document.createElement('div');
+
+  new BarChart(root, {
+    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    series: [
+      [5, 4, 3, 7, 5, 10, 3],
+      [3, 2, 9, 5, 4, 6, 4]
+    ]
+  }, {
+    seriesBarDistance: 10,
+    reverseData: true,
+    horizontalBars: true,
+    axisY: {
+      offset: 70
+    }
+  });
+
+  return root
+}
+
+export function Adaptive() {
   const root = document.createElement('div');
 
   new BarChart(root, {
@@ -56,26 +254,57 @@ export function Default() {
   return root
 }
 
-export function SimpleGrid() {
+export function OverlappingBarsOnMobile() {
   const root = document.createElement('div');
 
   new BarChart(root, {
-    series: [[
-      {x: 1, y: 1},
-      {x: 3, y: 5}
-    ]]
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    series: [
+      [5, 4, 3, 7, 5, 10, 3, 4, 8, 10, 6, 8],
+      [3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4]
+    ]
   }, {
+    seriesBarDistance: 10
+  }, [
+    ['screen and (max-width: 640px)', {
+      seriesBarDistance: 5,
+      axisX: {
+        labelInterpolationFnc(value) {
+          return value[0];
+        }
+      }
+    }]
+  ]);
+
+  return root
+}
+
+export function PeakCircles() {
+  const root = document.createElement('div');
+
+  new BarChart(root, {
+    labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
+    series: [
+      [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
+    ]
+  }, {
+    high: 10,
+    low: -10,
     axisX: {
-      type: AutoScaleAxis,
-      onlyInteger: true
-    },
-    axisY: {
-      type: AutoScaleAxis,
-      onlyInteger: true
+      labelInterpolationFnc(value, index) {
+        return index % 2 === 0 ? value : null;
+      }
     }
-  }).on('created', () => {
-    console.log(document.querySelectorAll('g.ct-grids line.ct-grid.ct-horizontal').length)
-    console.log(document.querySelectorAll('g.ct-grids line.ct-grid.ct-vertical').length)
+  }).on('draw', (data) => {
+    // If this draw event is of type bar we can use the data to create additional content
+    if(data.type === 'bar') {
+      // We use the group element of the current series to append a simple circle with the bar peek coordinates and a circle radius that is depending on the value
+      data.group.append(new Svg('circle', {
+        cx: data.x2,
+        cy: data.y2,
+        r: Math.abs(getMultiValue(data.value)) * 2 + 5
+      }, 'ct-slice-pie'));
+    }
   });
 
   return root
